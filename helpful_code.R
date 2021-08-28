f5d82da99619773853eefadb88ebdb43f23278aa
ernest.pedapati@cchmc.org <ernie@Ernies-MacBook-Air.local>
2021-08-28 05:53:42 -0400

    New instance config

---
+++ helpful_code.R
@@ -0,0 +1,8 @@
+## Useful Tutorial
+
+# https://happygitwithr.com/new-github-first.html#new-github-first
+
+# 'git remote -v' or git remote --verbose shows the remotes you have setup. 
+
+# git branch -vv prints info about the current branch.
+
